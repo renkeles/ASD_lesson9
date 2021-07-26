32576a5a2c50ca62e1c9168465a4e6b43aa4686d
renkeles <84261162+renkeles@users.noreply.github.com>
2021-07-26 08:26:21 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,6 +21,48 @@ void initStruct() {
     items = 0;
 }
 
+void insert(size_t _priority, size_t _data) {
+    nodeType* node = new nodeType;
+    node->data = _data;
+    node->priority = _priority;
+    size_t flag;
+
+    if (items == 0) {
+        arr[tail++] = node;
+        items++;
+    }
+    else if (items == SIZE_Q) {
+        std::cout << "Queue is full" << std::endl;
+        return;
+    }
+    else {
+        size_t i = 0;
+        size_t id = 0;
+        nodeType* temp;
+        for (i = 0; i < tail; i++) {
+            id = i % SIZE_Q;
+            if (arr[id]->priority > _priority) {
+                break;
+            }
+            else {
+                id++;
+            }
+        }
+        flag = id % SIZE_Q;
+        i++;
+        while (i <= tail) {
+            id = i % SIZE_Q;
+            temp = arr[id];
+            arr[id] = arr[flag];
+            arr[flag] = temp;
+            i++;
+        }
+        arr[flag] = node;
+        items++;
+        tail++;
+    }
+}
+
 
 
 int main()
