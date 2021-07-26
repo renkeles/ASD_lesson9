de3288092f6970e9f96fa7194062e0714977f2c0
renkeles <84261162+renkeles@users.noreply.github.com>
2021-07-26 09:19:04 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ void insert(size_t _priority, size_t _data) {
         size_t i = 0;
         size_t id = 0;
         nodeType* temp;
-        for (i = 0; i < tail; i++) {
+        for (i = 0; i < tail; ++i) {
             id = i % SIZE_Q;
             if (arr[id]->priority > _priority) {
                 break;
@@ -63,10 +63,56 @@ void insert(size_t _priority, size_t _data) {
     }
 }
 
+nodeType* remove() {
+    if (items == 0) {
+        return NULL;
+    } else{
+        size_t id = head++ % SIZE_Q;
+        nodeType* temp = arr[id];
+        arr[id] = NULL;
+        items--;
+        return temp;
+    }
+}
+
+void printQ(){
+    std::cout << "[ ";
+    for (size_t i = 0; i < SIZE_Q; i++) {
+        if (arr[i] == NULL) {
+            std::cout << "[*, *] ";
+        }
+        else {
+            std::cout << "[" << arr[i]->priority << ", " << arr[i]->data << "]";
+        }
+    }
+    std::cout << " ]" << std::endl;
+}
+
 
 
 int main()
 {
-
+    initStruct();
+    insert(1, 11);
+    insert(3, 22);
+    insert(4, 33);
+    insert(2, 44);
+    insert(3, 55);
+    insert(4, 66);
+    insert(5, 77);
+    insert(1, 88);
+    insert(2, 99);
+    insert(6, 100);
+    printQ();
+    for (size_t i = 0; i < 7; i++) {
+        nodeType* node = remove();
+        std::cout << "priority = " << node->priority << " data = " << node->data << std::endl;
+    }
+    printQ();
+    insert(1, 110);
+    insert(3, 120);
+    insert(6, 130);
+    printQ();
+    
     return 0;
 }
